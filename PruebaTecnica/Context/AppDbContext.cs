8ad095181d6d6b57c17b5358669e33889ec279ab
arielgonzalez <ariel.gonzalez@creativatechstudios.com>
2024-09-04 16:48:46 -0600

    First commit

---
+++ PruebaTecnica/Context/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PruebaTecnica.Models;
+
+namespace PruebaTecnica.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+
+        public DbSet<Person> Persons { get; set; }
+    }
+}
